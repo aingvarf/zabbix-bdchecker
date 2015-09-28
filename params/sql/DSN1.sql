e5be9e914bc3cbeda35b0c25912d08e638ea4f92
Alaev Igor <aingvarf@gmail.com>
2015-09-28 15:41:04 +0500

    Add description for parameters

--- params/sql/DSN1.sql
+++ params/sql/DSN1.sql
@@ -1,8 +1,14 @@
 # The name of this file should be the same as DSN name in databases.yml
 
+# Symbols ? are placeholders for parameters, which are applied according there order in request
+# For example:
+#   name='param'  => name=?
+#   name=param    => name=? 
+#   name in ('par1','par2') => name in (?)
+
 TEST: select 1 from dual
 
 # multiline sql
 SYSDATE: !
-  select sysdate
+  select sysdate - ?
   from dual
